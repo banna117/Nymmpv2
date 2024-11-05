f4ca14554fd05e0844afb3e42f5cfd70e9103818
banna117 <banna111222@gmail.com>
2024-11-05 16:04:20 +0900

    initialization

---
+++ src/main/java/com/example/nymmpv2/Nymmpv2Application.java
@@ -0,0 +1,13 @@
+package com.example.nymmpv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nymmpv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nymmpv2Application.class, args);
+	}
+
+}
